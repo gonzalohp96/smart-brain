cf56a1168a8b572a9fa9a8b82d7bd5545d9e47f1
Gonzalo <gonzalo@MacBook-Pro-de-Gonzalo-2.local>
2019-12-24 21:10:22 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -35,9 +35,26 @@ class App extends Component {
       imageUrl:'',
       box:{},
       route: 'Signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user:{
+        id:'',
+        name:'',
+        email:'',
+        entries:0,
+        joined:''
+      }
     }
   };
+
+  loadUser=(data)=>{
+    this.setState({user:{
+      id:data.id,
+      name:data.name,
+      email:data.email,
+      entries:data.entries,
+      joined:data.joined
+    }})
+  }
 
   calculateFaceLocation=(data)=>{
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -65,8 +82,25 @@ class App extends Component {
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL, //este no es el api key, es un model id
-        this.state.input)
-      .then(response=>this.displayFaceBox(this.calculateFaceLocation(response)))
+        this.state.input) 
+      .then(
+        response=>{
+          if (response){
+            fetch('http://localhost:3000/image',{
+              method:'put',
+              headers:{'Content-Type':'application/json'},
+              body: JSON.stringify({
+                id:this.state.user.id   
+              })
+            })
+            .then(response=>response.json())
+            .then(count=>{
+              this.setState(Object.assign(this.state.user,{entries:count}))  
+            })
+          } 
+          this.displayFaceBox(this.calculateFaceLocation(response))
+        }
+      )
       .catch(err=>console.log(err));
   };
 
@@ -88,14 +122,14 @@ class App extends Component {
         {route === 'home' 
         ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
             <FaceRecognition box={box} imageUrl={imageUrl}/>
           </div>
         : (
           route === 'Signin' 
-          ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
         }
       </div>
